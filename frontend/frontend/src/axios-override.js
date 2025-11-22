31ce8fc978fdd668136365e09206e3c475934afc
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-11-22 10:18:32 +0100

    mock GitHub API responses instead of finished cards

--- frontend/frontend/src/axios-override.js
+++ frontend/frontend/src/axios-override.js
@@ -1,5 +1,18 @@
 import axios from 'axios';
 import { setupCache } from 'axios-cache-interceptor';
+import { useSelector } from 'react-redux';
+import { HOST } from './constants';
+
+import additionalUserStars from './mockData/additional_user_stars.json' with { type: 'json' };
+import commentedIssues from './mockData/commented_issues.json' with { type: 'json' };
+import commentedPrs from './mockData/commented_prs.json' with { type: 'json' };
+import commits from './mockData/commits.json' with { type: 'json' };
+import gist from './mockData/gist.json' with { type: 'json' };
+import repository from './mockData/repository.json' with { type: 'json' };
+import reviewedPrs from './mockData/reviewed_prs.json' with { type: 'json' };
+import topLanguages from './mockData/top_languages.json' with { type: 'json' };
+import userStats from './mockData/user_stats.json' with { type: 'json' };
+import wakatimeProxy from './mockData/wakatime_proxy.json' with { type: 'json' };
 
 const cachedAxios = setupCache(axios, {
   // Cache for 30 minutes
@@ -9,24 +22,88 @@ const cachedAxios = setupCache(axios, {
   methods: ['get', 'post'],
 });
 
-// mock username=anuraghazra requests
+function createResolvedPromise(data, config) {
+  return Promise.resolve({
+    data,
+    status: 200,
+    statusText: 'OK',
+    headers: {},
+    config,
+  });
+}
+
+// mock responses to unauthenticated "anuraghazra" requests
 cachedAxios.interceptors.request.use(
   (config) => {
-    const params = new URL(config.url).entries || {};
-    if (
-      params.username === 'anuraghazra' ||
-      (params.repo && params.repo.startsWith('anuraghazra/'))
-    ) {
-      // Return a promise that resolves with a mocked response
-      return Promise.resolve({
-        data: 'hello anuraghazra!',
-        status: 200,
-        statusText: 'OK',
-        headers: {},
-        config,
-      });
+    const userId = useSelector((state) => state.user.userId);
+    const isAuthenticated = userId && userId.length > 0;
+    if (isAuthenticated) {
+      return config;
     }
-    return config;
+
+    const params = new URL(config.url).entries || {};
+
+    if (
+      config.url === 'https://api.github.com/graphql' &&
+      config.data.query.includes(
+        'query userInfo($login: String!, $after: String, $includeMergedPullRequests:',
+      ) &&
+      config.data.data.variables.login === 'anuraghazra'
+    ) {
+      return createResolvedPromise(userStats, config);
+    }
+
+    if (
+      config.url === 'https://api.github.com/graphql' &&
+      config.data.query.includes(
+        'query userInfo($login: String!, $after: String, $ownerAffiliations:',
+      ) &&
+      config.data.data.variables.login === 'anuraghazra'
+    ) {
+      return createResolvedPromise(additionalUserStars, config);
+    }
+
+    if (
+      config.url === 'https://api.github.com/graphql' &&
+      config.data.query.includes(
+        'query userInfo($login: String!, $ownerAffiliations:',
+      ) &&
+      config.data.data.variables.login === 'anuraghazra'
+    ) {
+      return createResolvedPromise(topLanguages, config);
+    }
+
+    if (
+      config.url === 'https://api.github.com/graphql' &&
+      config.data.query.includes('fragment RepoInfo on Repository {') &&
+      config.data.data.variables.login === 'anuraghazra' &&
+      config.data.data.variables.repo === 'github-readme-stats'
+    ) {
+      return createResolvedPromise(repository, config);
+    }
+
+    if (
+      config.url === 'https://api.github.com/graphql' &&
+      config.data.query.includes('query gistInfo(') &&
+      config.data.data.variables.login === 'anuraghazra'
+    ) {
+      return createResolvedPromise(gist, config);
+    }
+
+    switch (config.url) {
+      case 'https://api.github.com/search/commits?per_page=1&q=author:anuraghazra':
+        return createResolvedPromise(commits, config);
+      case 'https://api.github.com/search/issues?per_page=1&q=commenter:anuraghazra+-author:anuraghazra+type:pr':
+        return createResolvedPromise(commentedPrs, config);
+      case 'https://api.github.com/search/issues?per_page=1&q=reviewed-by:anuraghazra+-author:anuraghazra+type:pr':
+        return createResolvedPromise(reviewedPrs, config);
+      case 'https://api.github.com/search/issues?per_page=1&q=commenter:anuraghazra+-author:anuraghazra+type:issue':
+        return createResolvedPromise(commentedIssues, config);
+      case `https://${HOST}/api/wakatime-proxy?username=ffflabs`:
+        return createResolvedPromise(wakatimeProxy, config);
+      default:
+        return config;
+    }
   },
   (error) => {
     return Promise.reject(error);
--- frontend/frontend/src/components/Card/SVG.js
+++ frontend/frontend/src/components/Card/SVG.js
@@ -11,13 +11,6 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 import { createMockReq, createMockRes } from '../../mock-http';
 import { default as router } from '../../backend/.vercel/output/functions/api.func/router.js';
-import axios from 'axios';
-import { HOST } from '../../constants';
-import { DEMO_STATS_CARD } from './demoData/statsCard';
-import { DEMO_TOPLANGS_CARD } from './demoData/topLangsCard';
-import { DEMO_PIN_CARD } from './demoData/pinCard';
-import { DEMO_GIST_CARD } from './demoData/gistCard';
-import { DEMO_WAKATIME_CARD } from './demoData/wakatimeCard';
 
 const SvgInline = (props) => {
   const [svg, setSvg] = useState(null);
@@ -36,57 +29,32 @@ const SvgInline = (props) => {
       let body;
       let status;
 
-      switch (url) {
-        case `https://${HOST}/api?username=anuraghazra&include_all_commits=true&client=wizard`:
-          status = 200;
-          body = DEMO_STATS_CARD;
-          break;
-        case `https://${HOST}/api/top-langs?username=anuraghazra&langs_count=4&client=wizard`:
-          status = 200;
-          body = DEMO_TOPLANGS_CARD;
-          break;
-        case `https://${HOST}/api/pin?repo=anuraghazra/github-readme-stats&client=wizard`:
-          status = 200;
-          body = DEMO_PIN_CARD;
-          break;
-        case `https://${HOST}/api/gist?id=bbfce31e0217a3689c8d961a356cb10d&client=wizard`:
-          status = 200;
-          body = DEMO_GIST_CARD;
-          break;
-        case `https://${HOST}/api/wakatime?username=ffflabs&langs_count=6&card_width=450&client=wizard`:
-          status = 200;
-          body = DEMO_WAKATIME_CARD;
-          break;
+      const isAuthenticated = userId && userId.length > 0;
+      if (isAuthenticated && (!userToken || userToken === 'placeholderPAT')) {
+        // waiting for backend call to private-access
+        return;
+      }
 
-        default:
-          const isAuthenticated = userId && userId.length > 0;
-          if (
-            isAuthenticated &&
-            (!userToken || userToken === 'placeholderPAT')
-          ) {
-            // waiting for backend call to private-access
-            return;
-          }
-
-          if (isAuthenticated) {
-            const req = createMockReq({
-              method: 'GET',
-              url: url,
-            });
-            const res = createMockRes();
-            await router(req, res);
-            body = res._getBody();
-            status = res._getStatusCode();
+      // if (isAuthenticated) {
+      const req = createMockReq({
+        method: 'GET',
+        url: url,
+      });
+      const res = createMockRes();
+      await router(req, res);
+      body = res._getBody();
+      status = res._getStatusCode();
+      /*
           } else {
             let res = await axios.get(url);
             body = res.data;
             status = res.status;
           }
+*/
 
-          if (status >= 300) {
-            console.error('failed to fetch/generate SVG');
-            return;
-          }
+      if (status >= 300) {
+        console.error('failed to fetch/generate SVG');
+        return;
       }
 
       setSvg(body);
