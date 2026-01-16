6b7d8198c35b43c8999aab4e06cb8cd10aa1e1b7
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-01-16 19:47:57 +0100

    fix build on Vercel

--- vercel-preparation.sh
+++ vercel-preparation.sh
@@ -7,6 +7,9 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 mkdir -p backend/.vercel/output/functions/api.func/
-rsync -a --exclude '.vercel' backend/ backend/.vercel/output/functions/api.func/
+# copying `backend` to `backend/.vercel/...` directly may cause problems
+cp -R backend/. backend-copy/
+# `shopt` includes dot-files in the `mv` operation
+(shopt -s dotglob && mv backend-copy/* backend/.vercel/output/functions/api.func/)
 cp -R backend/.vercel/output/functions/api.func/_dot_vercel_copy/output backend/.vercel/
 cp -R backend frontend/frontend/src/backend/
