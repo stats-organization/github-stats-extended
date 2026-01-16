d3b836fb3dcb48191c06412db97a2cbf1ff4a298
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-01-16 18:51:38 +0100

    extend build documentation, add GH action, fix build on MacOS

--- vercel-preparation.sh
+++ vercel-preparation.sh
@@ -1,7 +1,12 @@
-# move into the folder of this script
-cd "$(dirname "$0")" || exit 1
+#!/bin/bash
 
-mkdir --parents backend/.vercel/output/functions/api.func/
-cp --recursive backend/* backend/.vercel/output/functions/api.func/
-cp --recursive backend/.vercel/output/functions/api.func/_dot_vercel_copy/output/ backend/.vercel/
-cp --recursive backend frontend/frontend/src/backend/
+# defensive error handling
+set -euo pipefail
+
+# move into the folder of this script
+cd "$(dirname "$0")"
+
+mkdir -p backend/.vercel/output/functions/api.func/
+rsync -a --exclude '.vercel' backend/ backend/.vercel/output/functions/api.func/
+cp -R backend/.vercel/output/functions/api.func/_dot_vercel_copy/output backend/.vercel/
+cp -R backend frontend/frontend/src/backend/
