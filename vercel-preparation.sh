5f51562dd83bbfbf1cd99bafc514ba93ff902dd2
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-02-05 21:37:52 +0100

    try fix Vercel frontend build 2

--- vercel-preparation.sh
+++ vercel-preparation.sh
@@ -10,9 +10,7 @@ mkdir -p apps/backend/.vercel/output/functions/api.func/
 # copying `backend` to `backend/.vercel/...` directly may cause problems
 cp -RP apps/backend/. apps/backend-copy/
 # `shopt` includes dot-files in the `mv` operation
-du -hc ./apps/backend-copy/ | grep "M"
 (shopt -s dotglob && mv apps/backend-copy/* apps/backend/.vercel/output/functions/api.func/)
 cp -RP apps/backend/.vercel/output/functions/api.func/_dot_vercel_copy/output apps/backend/.vercel/
 rm -rf apps/backend/node_modules
-du -hc ./apps/backend/ | grep "M"
 cp -RP apps/backend apps/frontend/src/backend/
