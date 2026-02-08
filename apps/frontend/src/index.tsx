789dd9572d902e136677d9f50c34401bf1502540
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-02-08 15:11:00 +0100

    fix(frontend): restore axios-override and convert index to ts

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,13 +1,14 @@
+import "./axios-override";
+
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { AppTrends } from "./pages/App/AppTrends";
 import { store } from "./redux/store";
 
-import "./axios-override";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <Provider store={store}>
