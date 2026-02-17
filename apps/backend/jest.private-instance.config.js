d07321dfc5d7e51c1e20f3f884ab120e64a6b653
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-02-17 16:30:48 +0100

    fixes for knip

--- apps/backend/jest.private-instance.config.js
+++ apps/backend/jest.private-instance.config.js
@@ -3,7 +3,10 @@ export default {
   transform: {},
   testEnvironment: "jsdom",
   modulePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.vercel/"],
-  testRegex: "\\.(private-instance|test)\\.(ts|tsx|js)$",
+  testMatch: [
+    "<rootDir>/tests/*.test.{ts,tsx,js}",
+    "<rootDir>/tests/private-instance/*.test.{ts,tsx,js}",
+  ],
   setupFiles: [
     "<rootDir>/tests/setup.jest.js",
     "<rootDir>/tests/setup.private-instance.jest.js",
--- apps/backend/jest.public-instance.config.js
+++ apps/backend/jest.public-instance.config.js
@@ -3,6 +3,9 @@ export default {
   transform: {},
   testEnvironment: "jsdom",
   modulePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.vercel/"],
-  testRegex: "\\.(public-instance|test)\\.(ts|tsx|js)$",
+  testMatch: [
+    "<rootDir>/tests/*.test.{ts,tsx,js}",
+    "<rootDir>/tests/public-instance/*.test.{ts,tsx,js}",
+  ],
   setupFiles: ["<rootDir>/tests/setup.jest.js"],
 };
