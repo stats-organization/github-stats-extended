dccc9105b28eed52f59a595af48921396f4c7a63
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-02-17 09:36:31 +0100

    apply suggestions for cleaner code

--- apps/backend/scripts/generate-langs-json.js
+++ apps/backend/scripts/generate-langs-json.js
@@ -2,30 +2,29 @@ import fs from "fs";
 
 import axios from "axios";
 import { default as jsYaml } from "js-yaml";
+import * as prettier from "prettier";
 
 const LANGS_FILEPATH = "./src/common/languageColors.json";
 
-//Retrieve languages from github linguist repository yaml file
-//@ts-ignore
-axios
-  .get(
-    "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml",
-  )
-  .then((response) => {
-    //and convert them to a JS Object
-    const languages = jsYaml.load(response.data);
+// Retrieve languages from github linguist repository yaml file
+const response = await axios.get(
+  "https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml",
+);
 
-    const languageColors = {};
+// and convert them to a JS Object
+const languages = jsYaml.load(response.data);
 
-    //Filter only language colors from the whole file
-    Object.keys(languages).forEach((lang) => {
-      languageColors[lang] = languages[lang].color;
-    });
+const languageColors = {};
 
-    //Debug Print
-    //console.dir(languageColors);
-    fs.writeFileSync(
-      LANGS_FILEPATH,
-      JSON.stringify(languageColors, null, "  "),
-    );
-  });
+// Filter only language colors from the whole file
+Object.keys(languages).forEach((lang) => {
+  languageColors[lang] = languages[lang].color;
+});
+
+// Debug Print
+// console.dir(languageColors);
+const jsonString = JSON.stringify(languageColors);
+fs.writeFileSync(
+  LANGS_FILEPATH,
+  await prettier.format(jsonString, { parser: "json" }),
+);
