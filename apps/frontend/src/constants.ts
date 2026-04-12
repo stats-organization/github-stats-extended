6113a79370c936fb9431ef121cb90eccf467f9f2
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-04-12 10:27:13 +0200

    cast DEMO_* constants to string

--- apps/frontend/src/constants.ts
+++ apps/frontend/src/constants.ts
@@ -13,10 +13,10 @@ const REDIRECT_URI = `https://${HOST}/frontend`;
 export const GITHUB_PRIVATE_AUTH_URL = `https://github.com/login/oauth/authorize?scope=user,repo&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}?mode=private`;
 export const GITHUB_PUBLIC_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}?mode=public`;
 
-export const DEMO_USER = "anuraghazra";
-export const DEMO_REPO = "anuraghazra/github-readme-stats";
-export const DEMO_GIST = "bbfce31e0217a3689c8d961a356cb10d";
-export const DEMO_WAKATIME_USER = "alan";
+export const DEMO_USER = "anuraghazra" as string;
+export const DEMO_REPO = "anuraghazra/github-readme-stats" as string;
+export const DEMO_GIST = "bbfce31e0217a3689c8d961a356cb10d" as string;
+export const DEMO_WAKATIME_USER = "alan" as string;
 
 window.process = {
   env: {
