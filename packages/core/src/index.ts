b32eddb8ed6b699499739bf32861838c97364f4f
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-04-15 15:56:41 +0200

    refactor: remove ts-expect-error comments

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,39 +1,25 @@
 /**
  * We need this file to be in ts to allow custom conditions to work
- * The disable comments are a temporary solution
+ * The package will be converted
  *
- * @see https://github.com/stats-organization/github-stats-extended/issues/140
+ * @todo https://github.com/stats-organization/github-stats-extended/issues/140
  */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
-// @ts-expect-error
 export { fetchWakatimeStats } from "./fetchers/wakatime.js";
-// @ts-expect-error
 export { retryer } from "./common/retryer.js";
 
-// @ts-expect-error
 export { renderError } from "./common/render.js";
 
-// @ts-expect-error
 export { dateDiff, clampValue } from "./common/ops.js";
 
-// @ts-expect-error
 export { logger } from "./common/log.js";
-// @ts-expect-error
 export { request } from "./common/http.js";
 
-// @ts-expect-error
 export { default as gist } from "./api/gist.js";
-// @ts-expect-error
 export { default as api } from "./api/index.js";
-// @ts-expect-error
 export { default as pin } from "./api/pin.js";
-// @ts-expect-error
 export { default as topLangs } from "./api/top-langs.js";
-// @ts-expect-error
 export { default as wakatime } from "./api/wakatime.js";
 
-// @ts-expect-error
 export { getConfig } from "./common/config.js";
 
 export { themes } from "./themes/index.js";
