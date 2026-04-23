51cd0a8ad0e70487ca32e3e60a682912c9c264fb
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-04-23 21:31:46 +0200

    undo build.sh creation

--- apps/backend/build.sh
+++
@@ -1,6 +0,0 @@
-cd ../../
-git clean ./apps -fx
-pnpm run build:packages
-pnpm --filter ./apps/backend/ --legacy --prod deploy ./apps/deployment/
-mv ./apps/deployment/node_modules/ ./apps/backend/node_modules/
-./vercel-preparation.sh
