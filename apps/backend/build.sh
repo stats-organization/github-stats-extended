df43e96ed5a4ae169bf292fc02d2efb7afc3ce6e
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-04-23 18:33:53 +0200

    prod deployment, remove build command

--- apps/backend/build.sh
+++ apps/backend/build.sh
@@ -1,6 +1,6 @@
 cd ../../
 git clean ./apps -fx
 pnpm run build:packages
-pnpm --filter ./apps/backend/ --legacy deploy ./apps/deployment/
+pnpm --filter ./apps/backend/ --legacy --prod deploy ./apps/deployment/
 mv ./apps/deployment/node_modules/ ./apps/backend/node_modules/
 ./vercel-preparation.sh
