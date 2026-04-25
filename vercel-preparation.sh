bf6f390dbae233881d0dfff594ffa6149ef0dde4
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2026-04-25 09:37:35 +0200

    remove deletion commands, add 'pnpm install'

--- vercel-preparation.sh
+++ vercel-preparation.sh
@@ -12,7 +12,7 @@ cp -RP apps/backend/. apps/backend-copy/
 # `shopt` includes dot-files in the `mv` operation
 (shopt -s dotglob && mv apps/backend-copy/* apps/backend/.vercel/output/functions/api.func/)
 cp -RP apps/backend/.vercel/output/functions/api.func/_dot_vercel_copy/output apps/backend/.vercel/
-rm -rf apps/backend/node_modules
 rm -rf apps/deployment
+pnpm install
 pnpm build:frontend
 cp -RP apps/frontend/build/. apps/backend/.vercel/output/static/
