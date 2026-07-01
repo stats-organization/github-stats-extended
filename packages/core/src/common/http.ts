3ad6a62433253b319b6cb72288282e5c7bc34d4b
Marco Pasqualetti <marco.pasqualetti@live.com>
2026-07-01 22:09:04 +0200

    refactor(core): type data of `request` with `GraphQLRequest`

--- packages/core/src/common/http.ts
+++ packages/core/src/common/http.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 import type { AxiosRequestConfig, AxiosResponse } from "axios";
 
+/** Body of a GraphQL request sent to the GitHub API. */
+interface GraphQLRequest {
+  /** The GraphQL query. */
+  query: string;
+  /** Variables referenced by the query. */
+  variables: Record<string, unknown>;
+}
+
 /**
  * Send GraphQL request to GitHub API.
  *
@@ -9,7 +17,7 @@ import type { AxiosRequestConfig, AxiosResponse } from "axios";
  * @returns Request response.
  */
 const request = (
-  data: unknown,
+  data: GraphQLRequest,
   headers: NonNullable<AxiosRequestConfig["headers"]>,
 ): Promise<AxiosResponse> => {
   return axios({
